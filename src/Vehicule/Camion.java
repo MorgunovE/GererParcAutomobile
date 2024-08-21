ccf6f6923c62d4d70f86aaed7c34f7b08278ffcc
Evgenii Morgunov <MorgunovE87@gmail.com>
2024-08-21 10:37:50 -0400

    add doc in class Camion

--- src/Vehicule/Camion.java
+++ src/Vehicule/Camion.java
@@ -1,9 +1,18 @@
 package Vehicule;
 
-
+/**
+ * Classe Camion
+ */
 public class Camion extends Vehicule {
     private int capaciteDeChargement;
 
+    /**
+     * Constructeur de la classe Camion
+     * @param id L'identifiant du camion
+     * @param modele Le modèle du camion
+     * @param marque La marque du camion
+     * @param capaciteDeChargement La capacité de chargement du camion
+     */
     public Camion(String id, String modele, String marque, int capaciteDeChargement) {
         super(id, modele, marque);
         if (capaciteDeChargement <= 0) {
@@ -12,10 +21,18 @@ public class Camion extends Vehicule {
         this.capaciteDeChargement = capaciteDeChargement;
     }
 
+    /**
+     * Méthode getCapaciteDeChargement
+     * @return La capacité de chargement du camion
+     */
     public int getCapaciteDeChargement() {
         return capaciteDeChargement;
     }
 
+    /**
+     * Méthode toString
+     * @return La représentation textuelle du camion
+     */
     @Override
     public String toString() {
         return super.toString() + ", Capacité: " + capaciteDeChargement + " kg";
