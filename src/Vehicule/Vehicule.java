9fa364eb07a958425c53597fcc05ff85f019a8d0
Evgenii Morgunov <MorgunovE87@gmail.com>
2024-08-21 10:36:37 -0400

    add doc in class Vehicule

--- src/Vehicule/Vehicule.java
+++ src/Vehicule/Vehicule.java
@@ -1,12 +1,21 @@
 package Vehicule;
 import static java.lang.Integer.parseInt;
 
+/**
+ * Classe abstraite Vehicule
+ */
 public abstract class Vehicule {
     private String id;
     private String modele;
     private String marque;
     private boolean estLoue;
 
+    /**
+     * Constructeur de la classe Vehicule
+     * @param id L'identifiant du véhicule
+     * @param modele Le modèle du véhicule
+     * @param marque La marque du véhicule
+     */
     public Vehicule(String id, String modele, String marque) {
         if (parseInt(id) <= 0) {
             throw new IllegalArgumentException("L'ID doit être supérieur à 0");
@@ -23,26 +32,50 @@ public abstract class Vehicule {
         this.estLoue = false;
     }
 
+    /**
+     * Méthode abstraite toString
+     * @return La représentation textuelle du véhicule
+     */
     public String getId() {
         return id;
     }
 
+    /**
+     * Méthode abstraite toString
+     * @return La représentation textuelle du véhicule
+     */
     public String getModele() {
         return modele;
     }
 
+    /**
+     * Méthode abstraite toString
+     * @return La représentation textuelle du véhicule
+     */
     public String getMarque() {
         return marque;
     }
 
+    /**
+     * Méthode abstraite toString
+     * @return La représentation textuelle du véhicule
+     */
     public boolean isEstLoue() {
         return estLoue;
     }
 
+    /**
+     * Méthode abstraite toString
+     * @return La représentation textuelle du véhicule
+     */
     public void setEstLoue(boolean estLoue) {
         this.estLoue = estLoue;
     }
 
+    /**
+     * Méthode abstraite toString
+     * @return La représentation textuelle du véhicule
+     */
     @Override
     public String toString() {
         return "ID: " + id + ", Modèle: " + modele + ", Marque: " + marque;
