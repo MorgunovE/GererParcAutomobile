7c893fc0452665f7283882b252c2f9fbc1d22762
Evgenii Morgunov <MorgunovE87@gmail.com>
2024-08-21 10:38:21 -0400

    add doc in class Moto

--- src/Vehicule/Moto.java
+++ src/Vehicule/Moto.java
@@ -1,9 +1,18 @@
 package Vehicule;
 
-
+/**
+ * Classe Moto
+ */
 public class Moto extends Vehicule {
     private int cylindree;
 
+    /**
+     * Constructeur de la classe Moto
+     * @param id L'identifiant de la moto
+     * @param modele Le modèle de la moto
+     * @param marque La marque de la moto
+     * @param cylindree La cylindrée de la moto
+     */
     public Moto(String id, String modele, String marque, int cylindree) {
         super(id, modele, marque);
         if (cylindree <= 0) {
@@ -12,10 +21,18 @@ public class Moto extends Vehicule {
         this.cylindree = cylindree;
     }
 
+    /**
+     * Méthode getCylindree
+     * @return La cylindrée de la moto
+     */
     public int getCylindree() {
         return cylindree;
     }
 
+    /**
+     * Méthode toString
+     * @return La représentation textuelle de la moto
+     */
     @Override
     public String toString() {
         return super.toString() + ", Cylindrée: " + cylindree + " cc";
