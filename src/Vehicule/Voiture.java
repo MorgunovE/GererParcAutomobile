e17881882d16f7717cb865850dde353970804bc8
Evgenii Morgunov <MorgunovE87@gmail.com>
2024-08-21 10:37:15 -0400

    add doc in class Voiture

--- src/Vehicule/Voiture.java
+++ src/Vehicule/Voiture.java
@@ -1,9 +1,18 @@
 package Vehicule;
 
-
+/**
+ * Classe Voiture
+ */
 public class Voiture extends Vehicule {
     private int nombreDePortes;
 
+    /**
+     * Constructeur de la classe Voiture
+     * @param id L'identifiant de la voiture
+     * @param modele Le modèle de la voiture
+     * @param marque La marque de la voiture
+     * @param nombreDePortes Le nombre de portes de la voiture
+     */
     public Voiture(String id, String modele, String marque, int nombreDePortes) {
         super(id, modele, marque);
         if (nombreDePortes <= 0) {
@@ -12,10 +21,18 @@ public class Voiture extends Vehicule {
         this.nombreDePortes = nombreDePortes;
     }
 
+    /**
+     * Méthode getNombreDePortes
+     * @return Le nombre de portes de la voiture
+     */
     public int getNombreDePortes() {
         return nombreDePortes;
     }
 
+    /**
+     * Méthode toString
+     * @return La représentation textuelle de la voiture
+     */
     @Override
     public String toString() {
         return super.toString() + ", Nombre de portes: " + nombreDePortes;
