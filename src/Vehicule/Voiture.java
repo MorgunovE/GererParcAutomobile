7e4e97d3744f6ce30029c3c3e029697f586c7223
Evgenii Morgunov <MorgunovE87@gmail.com>
2024-08-21 08:58:57 -0400

    add public class Voiture extends Vehicule and code

---
+++ src/Vehicule/Voiture.java
@@ -0,0 +1,23 @@
+package Vehicule;
+
+
+public class Voiture extends Vehicule {
+    private int nombreDePortes;
+
+    public Voiture(int id, String modele, String marque, int nombreDePortes) {
+        super(id, modele, marque);
+        if (nombreDePortes <= 0) {
+            throw new IllegalArgumentException("Le nombre de portes doit être supérieur à 0");
+        }
+        this.nombreDePortes = nombreDePortes;
+    }
+
+    public int getNombreDePortes() {
+        return nombreDePortes;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + ", Nombre de portes: " + nombreDePortes;
+    }
+}
